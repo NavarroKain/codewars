d2d8bedb57255c243360dba822198157ae337149
NavarroKain <navarrokain@gmail.com>
2017-02-14 10:35:46 +0200

    some optimize

--- src/shortest_word.js
+++ src/shortest_word.js
@@ -1,7 +1,7 @@
 function findShort(s){
 	var arrayOfStrings = s.split(" ");
 	var minWord = arrayOfStrings[0].length;;
-	for ( var i=0; i<arrayOfStrings.length; i=i+1){
+	for ( var i=1; i<arrayOfStrings.length; i=i+1){
 		var length = arrayOfStrings[i].length;
 		if (length > 0 && minWord > length) {
 			minWord = length;
