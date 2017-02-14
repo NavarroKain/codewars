56972f5c11d80af1a553cbda1508076f3a7b8bb5
NavarroKain <navarrokain@gmail.com>
2017-02-14 10:31:24 +0200

    more tests

--- src/shortest_word.js
+++ src/shortest_word.js
@@ -1,9 +1,9 @@
 function findShort(s){
 	var arrayOfStrings = s.split(" ");
-	var minWord = 1000000;
-	for ( var i=1; i<arrayOfStrings.length; i=i+1){
+	var minWord = arrayOfStrings[0].length;;
+	for ( var i=0; i<arrayOfStrings.length; i=i+1){
 		var length = arrayOfStrings[i].length;
-		if (minWord > length) {
+		if (length > 0 && minWord > length) {
 			minWord = length;
 		}
 	}
--- tests/shortest_word.js
+++ tests/shortest_word.js
@@ -2,4 +2,7 @@ QUnit.test( "shortest word length", function( assert ) {
   assert.strictEqual(findShort("bitcoin take over the world maybe who knows perhaps"), 3);
   assert.strictEqual(findShort("turns out random test cases are easier than writing out basic ones"), 3);
   assert.strictEqual(findShort("hi some  word"), 2);
+  assert.strictEqual(findShort("small world with me"), 2);
+  assert.strictEqual(findShort("one"), 3);
+  assert.strictEqual(findShort(""), 0);
 });
