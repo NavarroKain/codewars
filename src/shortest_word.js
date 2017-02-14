e011af1d6e6a3bc46f3da59b7502663001fb8ab3
NavarroKain <navarrokain@gmail.com>
2017-02-14 10:19:18 +0200

    passsed test findShort

--- src/shortest_word.js
+++ src/shortest_word.js
@@ -1,3 +1,12 @@
 function findShort(s){
+	var arrayOfStrings = s.split(" ");
+	var minWord = 1000000;
+	for ( var i=1; i<arrayOfStrings.length; i=i+1){
+		var length = arrayOfStrings[i].length;
+		if (minWord > length) {
+			minWord = length;
+		}
+	}
 
+	return minWord;
 }
