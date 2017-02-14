43da05d1c95fe7dd84dbb8ad3aee2af473410370
NavarroKain <navarrokain@gmail.com>
2017-02-14 09:44:39 +0200

    test before code

---
+++ src/shortest_word.js
@@ -0,0 +1,3 @@
+function findShort(s){
+
+}
---
+++ tests/shortest_word.js
@@ -0,0 +1,4 @@
+QUnit.test( "shortest word length", function( assert ) {
+  assert.strictEqual(findShort("bitcoin take over the world maybe who knows perhaps"), 3);
+  assert.strictEqual(findShort("turns out random test cases are easier than writing out basic ones"), 3);
+});
